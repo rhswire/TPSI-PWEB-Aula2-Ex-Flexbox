d63d0fa9c180facd75181f04c94d23b2a8d757e9
Raquel Guimaraes <rhswire@gmail.com>
2022-10-11 15:49:04 +0100

    estrutura + flex direction wrap

---
+++ js/flex.js
@@ -0,0 +1,8 @@
+
+function flex(valor){
+    document.getElementById('container').style.flexDirection=valor;
+}
+
+function wrap(valor){
+    document.getElementById('container').style.flexWrap=valor;
+}
